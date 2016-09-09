50b224726262741c2c9b950e57f219b98d0396e5
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2016-09-09 18:22:39 +0200

    Travis config.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Iteraptor.Mixfile do
   def project do
     [app: :iteraptor,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
