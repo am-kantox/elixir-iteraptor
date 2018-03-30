ab4db13a9867ad3d5b2ac43b961155bf05d56203
Aleksei Matiushkin <am@mudasobwa.ru>
2018-03-30 19:57:47 +0200

    proper quoting instead of eval, packages update.

--- lib/iteraptor/iteraptable.ex
+++ lib/iteraptor/iteraptable.ex
@@ -26,49 +26,55 @@ defmodule Iteraptor.Iteraptable do
   """
 
   @codepieces [
-    enumerable: """
+    enumerable:
+      quote do
 
-      defimpl Enumerable, for: ★ do
-        def count(map) do
-          {:ok, map |> Map.from_struct |> map_size} # do not count :__struct__
+        defimpl Enumerable, for: __MODULE__ do
+          def slice(enumerable) do
+            {:error, __MODULE__}
+          end
+
+          def count(map) do
+            {:ok, map |> Map.from_struct |> map_size} # do not count :__struct__
+          end
+
+          def member?(_, {:__struct__, _}) do
+            {:ok, false}
+          end
+
+          def member?(map, {key, value}) do
+            {:ok, match?({:ok, ^value}, :maps.find(key, map))}
+          end
+
+          def member?(_, _) do
+            {:ok, false}
+          end
+
+          def reduce(map, acc, fun) do
+            do_reduce((map |> Map.from_struct |> :maps.to_list), acc, fun)
+          end
+
+          defp do_reduce(_,       {:halt, acc}, _fun),   do: {:halted, acc}
+          defp do_reduce(list,    {:suspend, acc}, fun), do: {:suspended, acc, &do_reduce(list, &1, fun)}
+          defp do_reduce([],      {:cont, acc}, _fun),   do: {:done, acc}
+          defp do_reduce([h | t], {:cont, acc}, fun),    do: do_reduce(t, fun.(h, acc), fun)
         end
 
-        def member?(_, {:__struct__, _}) do
-          {:ok, false}
+      end,
+    collectable:
+      quote do
+
+        defimpl Collectable, for: __MODULE__ do
+          def into(original) do
+            {original, fn
+              map, {:cont, {k, v}} -> :maps.put(k, v, map)
+              map, :done -> map
+              _,   :halt -> :ok
+            end}
+          end
         end
 
-        def member?(map, {key, value}) do
-          {:ok, match?({:ok, ^value}, :maps.find(key, map))}
-        end
-
-        def member?(_, _) do
-          {:ok, false}
-        end
-
-        def reduce(map, acc, fun) do
-          do_reduce((map |> Map.from_struct |> :maps.to_list), acc, fun)
-        end
-
-        defp do_reduce(_,       {:halt, acc}, _fun),   do: {:halted, acc}
-        defp do_reduce(list,    {:suspend, acc}, fun), do: {:suspended, acc, &do_reduce(list, &1, fun)}
-        defp do_reduce([],      {:cont, acc}, _fun),   do: {:done, acc}
-        defp do_reduce([h | t], {:cont, acc}, fun),    do: do_reduce(t, fun.(h, acc), fun)
       end
-
-    """,
-    collectable: """
-
-      defimpl Collectable, for: ★ do
-        def into(original) do
-          {original, fn
-            map, {:cont, {k, v}} -> :maps.put(k, v, map)
-            map, :done -> map
-            _,   :halt -> :ok
-          end}
-        end
-      end
-
-    """
   ]
 
   @doc """
@@ -87,13 +93,10 @@ defmodule Iteraptor.Iteraptable do
       %Iteraptor.Struct{field: 84}
   """
   defmacro __using__(opts \\ []) do
-    code = ([:enumerable, :collectable] |> Enum.reduce("", fn type, acc ->
-      acc <> (if opts[:skip] == type, do: "", else: @codepieces[type])
-    end))
-    quote do
-      impls = unquote(code) |> String.replace("★", to_string(__MODULE__))
-      Code.eval_string(impls, [], __ENV__)
-    end
+    [:enumerable, :collectable]
+    |> Enum.reduce([], fn type, acc ->
+      if opts[:skip] == type, do: acc, else: [@codepieces[type] | acc]
+    end)
   end
 end
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Iteraptor.Mixfile do
   def project do
     [app: :iteraptor,
      version: File.read!(Path.join("config", "VERSION")),
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
@@ -32,7 +32,7 @@ defmodule Iteraptor.Mixfile do
     [
       {:issuer, "~> 0.1", only: :dev},
 
-      {:credo, "~> 0.4", only: [:dev, :test]},
+      {:credo, "~> 0.8", only: [:dev, :test]},
       {:ex_doc, "~> 0.11", only: :dev},
       {:inch_ex, "~> 0.0", only: :docs}
     ]
