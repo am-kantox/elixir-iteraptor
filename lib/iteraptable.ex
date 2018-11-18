4f8b5809682bfbd9dcd0dc7afbef1c00f17c48b0
Aleksei Matiushkin <am@mudasobwa.ru>
2018-11-18 08:27:41 +0100

    Date and Time work.

---
+++ lib/iteraptable.ex
@@ -0,0 +1,33 @@
+defprotocol Iteraptable do
+  @moduledoc """
+  The protocol specifying how the respective struct might be used within `Iteraptor`.
+  """
+
+  @spec type(term :: any()) :: atom()
+  @doc "Returns a type understood by `Iteraptable`"
+  def type(term)
+
+  @spec to_enumerable(term :: any()) :: Enumerable.t
+  @doc "Converts a term to an enumerable"
+  def to_enumerable(term)
+
+  @spec to_collectable(term :: any()) :: Collectable.t
+  @doc "Converts a term to an collectable"
+  def to_collectable(term)
+
+  # @spec name(term :: any()) :: binary()
+  # @doc "Returns a name of the term to be represented in flatmaps"
+  # def name(term)
+end
+
+defimpl Iteraptable, for: Date do
+  def type(_term), do: Date
+  def to_enumerable(term), do: %{s·date: Date.to_iso8601(term)}
+  def to_collectable(_term), do: %{}
+end
+
+defimpl Iteraptable, for: Time do
+  def type(_term), do: Time
+  def to_enumerable(term), do: %{s·time: Time.to_iso8601(term)}
+  def to_collectable(_term), do: %{}
+end
