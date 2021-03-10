506b2307e573f4ff150ab50654a2da7bdd782afb
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-03-10 05:36:27 +0100

    Satisfy dialyzer/formatter for 1.11

---
+++ .iex.exs
@@ -0,0 +1,44 @@
+global_settings = "~/.iex.exs"
+if File.exists?(global_settings), do: Code.require_file(global_settings)
+
+Application.put_env(:elixir, :ansi_enabled, true)
+
+IEx.configure(
+  inspect: [limit: :infinity],
+  colors: [
+    eval_result: [:cyan, :bright],
+    eval_error: [[:red, :bright, "\n▶▶▶\n"]],
+    eval_info: [:yellow, :bright],
+    syntax_colors: [
+      number: :red,
+      atom: :blue,
+      string: :green,
+      boolean: :magenta,
+      nil: :magenta,
+      list: :white
+    ]
+  ],
+  default_prompt:
+    [
+      # cursor ⇒ column 1
+      "\e[G",
+      :blue,
+      "%prefix",
+      :magenta,
+      "|itr|",
+      :blue,
+      "%counter",
+      " ",
+      :yellow,
+      "▶",
+      :reset
+    ]
+    |> IO.ANSI.format()
+    |> IO.chardata_to_string()
+)
+
+defmodule S do
+  defstruct dt: DateTime.utc_now(), map: %{foo: 42, bar: %{baz: 73}}
+end
+
+import Iteraptor.AST
